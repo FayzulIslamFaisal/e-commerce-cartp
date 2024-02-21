ac081bebdbd067be718837bafebf3fb1f8572da1
FayzulIslamFaisal <mfbfaisal@gmail.com>
2024-02-21 22:05:00 +0600

    header footer product and prodeuct details page done

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,10 @@
 <?php
 
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\AppController;
+use App\Http\Controllers\ShopController;
+use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +18,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/',[AppController::class,'index'])->name('app.index');
+Route::get('/shop',[ShopController::class,'index'])->name('shop.index');
+Route::get('/details/{id}',[ShopController::class,'productDetails'])->name('shop.product.details');
+
+Auth::routes();
+
+
+// user middleware
+Route::middleware('auth')->group(function(){
+    Route::get('/myaccount',[UserController::class,'index'])->name('users.index');
+});
+// admin middleware
+Route::middleware('auth','auth.admin')->group( function(){
+    Route::get('/admin',[AdminController::class,'index'])->name('admin.index');
 });
