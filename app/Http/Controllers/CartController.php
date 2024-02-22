6748e4080d8211df81d4a955f70b90717992d790
FayzulIslamFaisal <mfbfaisal@gmail.com>
2024-02-22 09:04:15 +0600

    product add to cart with shopping cart package

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -2,9 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
+use Gloudemans\Shoppingcart\Facades\Cart;
 use Illuminate\Http\Request;
 
 class CartController extends Controller
 {
-    //
+    public function index() {
+
+        $cartItems = Cart::instance('cart')->content();
+        return view('cart.cartitem',compact('cartItems'));
+    }
+
+    public function addToCart(Request $request){
+        // dd($request);
+        $product = Product::find($request->id);
+        $price = $product->sale_price ? $product->sale_price : $product->regular_price;
+        Cart::instance('cart')->add($product->id, $product->name, $product->quantity,$price )->associate('App\Models\Product');
+        return redirect()->route('cart.index')->with('message','Product Item Successfully Add To Cart');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\AppController;
+use App\Http\Controllers\CartController;
 use App\Http\Controllers\ShopController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Auth;
@@ -19,8 +20,15 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/',[AppController::class,'index'])->name('app.index');
+
+// product shopping route
 Route::get('/shop',[ShopController::class,'index'])->name('shop.index');
 Route::get('/details/{id}',[ShopController::class,'productDetails'])->name('shop.product.details');
+// product Cart route
+Route::get('/cart',[CartController::class,'index'])->name('cart.index');
+Route::post('/cart/store',[CartController::class,'addToCart'])->name('cart.store');
+
+
 
 Auth::routes();
 
